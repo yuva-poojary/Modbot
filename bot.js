5673bb1bc475dae0ddc2c1fcefb832c7b0a6f76f
Qyther <32208907+Qyther@users.noreply.github.com>
2018-03-31 10:12:07 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,82 @@
+var Discord = require("discord.js");
+var prefix = "t!";
+var client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("ready to rumble!");
+});
+
+var bannedwords = "fuck,shit,slut,whore".split(",");
+
+client.on("message", msg => {
+  if (msg.guild === null) return;
+
+  for (i=0;i<bannedwords.length;i++) {
+    if (msg.content.toLowerCase().includes(bannedwords[i])) {
+      msg.delete();
+      msg.reply("Please don't swear!");
+      return;
+    }
+  }
+
+  if (msg.author.bot) return;
+  if (!msg.member.hasPermission("ADMINISTRATOR")) return;
+
+  if (!msg.content.toLowerCase().startsWith(prefix)) return;
+  msg.delete();
+  if (msg.content.toLowerCase().startsWith(prefix + "kick ")) {
+    var mem = msg.mentions.members.first();
+    mem.kick().then(() => {
+      msg.channel.send(mem.displayName + " has successfully been kicked by " + msg.author.username + "!");
+    }).catch(e => {
+      msg.channel.send("An error occured!");
+    });
+  }
+  if (msg.content.toLowerCase().startsWith(prefix + "ban ")) {
+    var mem = msg.mentions.members.first();
+    var mc = msg.content.split(" ")[2];
+    mem.ban(mc).then(() => {
+      msg.channel.send(mem.displayName + " has successfully been banned by " + msg.author.username + " for " + mc + " days!");
+    }).catch(e => {
+      msg.channel.send("An error occured!");
+    });
+  }
+  if (msg.content.toLowerCase().startsWith(prefix + "mute")) {
+    var mem = msg.mentions.members.first();
+    if (msg.guild.roles.find("name", "Muted")) {
+      mem.addRole(msg.guild.roles.find("name", "Muted")).then(() => {
+        msg.channel.send(mem.displayName + " has successfully been muted!");
+      }).catch(e => {
+        msg.channel.send("An error occured!");
+        console.log(e);
+      });
+
+    }
+  }
+  if (msg.content.toLowerCase().startsWith(prefix + "unmute")) {
+    var mem = msg.mentions.members.first();
+    if (msg.guild.roles.find("name", "Muted")) {
+      mem.removeRole(msg.guild.roles.find("name", "Muted")).then(() => {
+        msg.channel.send(mem.displayName + " has successfully been unmuted!");
+      }).catch(e => {
+        msg.channel.send("An error occured!");
+        console.log(e);
+      });
+
+    }
+  }
+  if (msg.content.toLowerCase().startsWith(prefix + "purge")) {
+    var mc = msg.content.split(" ")[1];
+    msg.channel.bulkDelete(mc);
+  }
+  if (msg.content.toLowerCase().startsWith(prefix + "eval")) {
+    var sc = msg.content.substring(msg.content.indexOf(" "));
+    eval(sc);
+  }
+  if (msg.content.toLowerCase().startsWith(prefix + "calc")) {
+    var ca = msg.content.substring(msg.content.indexOf(" "));
+    msg.reply(ca + " is " + eval(ca).toFixed(2));
+  }
+});
+
+client.login(Your token here!);
